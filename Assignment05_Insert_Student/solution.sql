6d51cf554fd3734d840ce1f1e521db5490d86bea
sanjai0000 <sanjai4943@gmail.com>
2026-08-17 12:49:51 +0530

    Update solution.sql

--- Assignment05_Insert_Student/solution.sql
+++ Assignment05_Insert_Student/solution.sql
@@ -1,9 +1,13 @@
--- Insert
-
--- 1001 Arun
-
--- 1002 Divya
-
--- 1003 Karthik
-
--- Display all records
+CREATE TABLE Student (
+StudentID INT(5)PRIMARY KEY,
+StudentName VARCHAR(20)NOT NULL,
+DOB DATE NOT NULL,
+Gender VARCHAR(10)NOT NULL,
+DepartmentID INT(5)NOT NULL
+);
+INSERT INTO Student(studentID,StudentName,Gender,DepartmentID)
+VALUES
+(1001,'Arun','Male',101),
+(1002,'Divya','Female',102),
+(1003,'Karthick','Male',101);
+SELECT*FROM Student;
