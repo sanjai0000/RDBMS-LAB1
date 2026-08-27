5b62f0ddabc035472616e83c930eac256dce42a9
sanjai0000 <sanjai4943@gmail.com>
2026-08-27 11:10:06 +0530

    Update solution.sql

--- Assignment08_Aggregate_Functions/solution.sql
+++ Assignment08_Aggregate_Functions/solution.sql
@@ -1,11 +1,30 @@
--- Create Employee table
+CREATE TABLE Employee (
+    EmployeeID INT,
+    EmployeeName VARCHAR(50),
+    Department VARCHAR(50),
+    Salary INT
+);
 
--- Insert records
-
+INSERT INTO Employee (EmployeeID, EmployeeName, Department, Salary)
+VALUES
+(101, 'Ravi', 'HR', 25000),
+(102, 'Meena', 'IT', 40000),
+(103, 'Kumar', 'Finance', 35000),
+(104, 'Suresh', 'IT', 45000),
+(105, 'Latha', 'HR', 30000);
 -- COUNT()
+SELECT COUNT(Salary) AS Total_Employees
+FROM Employee;
 
 -- MAX()
+SELECT MAX(Salary) AS Maximum_Salary
+FROM Employee;
 
 -- MIN()
+SELECT MIN(Salary) AS Minimum_Salary
+FROM Employee;
 
 -- AVG()
+SELECT AVG(Salary) AS Average_Salary
+FROM Employee;
+
